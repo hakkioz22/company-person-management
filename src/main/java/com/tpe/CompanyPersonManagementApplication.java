6ccaa6f2224c94f26acd5dfd4063eb1d238d1e73
hakkioz22 <hakkioz@outlook.com>
2023-04-17 03:14:45 +0300

    Initial Commit

--- src/main/java/com/tpe/CompanyPersonManagementApplication.java
+++ src/main/java/com/tpe/CompanyPersonManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompanyPersonalManagementApplication {
+public class CompanyPersonManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyPersonalManagementApplication.class, args);
+		SpringApplication.run(CompanyPersonManagementApplication.class, args);
 	}
 
 }
